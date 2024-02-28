e5c76471b3a57c6bee1d93f9583bc2ac3cceaf83
Athul Reji <athulreji007@gmail.com>
2024-02-29 01:46:03 +0530

    Implemented basic group chat

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 type Message struct {
 	Type       string `json:"type"`
 	IsGroupMsg bool   `json:"isGroupMsg"`
+	Group      string `json:"group"`
 	To         string `json:"to"`
 	Content    string `json:"content"`
 	From       string `json:"From"`
@@ -42,20 +43,44 @@ func main() {
 		return
 	}
 
-	// Listen for messages and send messages
-	fmt.Println("Choose option:")
-	fmt.Println("1. Direct Message")
-	fmt.Println("2. Group Message")
+	for true {
+		// Listen for messages and send messages
+		fmt.Println("Choose option:")
+		fmt.Println("1. Direct Message")
+		fmt.Println("2. Group Message")
+		fmt.Println("3. Create Group")
+		fmt.Println("4. Join Group")
 
-	var option int
+		var option int
 
-	fmt.Scanf("%d", &option)
+		fmt.Scanf("%d", &option)
 
-	if option == 1 {
-		directMessage(conn, username, reader)
-	} else {
-		groupMessage(conn, username, reader)
+		if option == 1 {
+			directMessage(conn, username, reader)
+		} else if option == 2 {
+			groupMessage(conn, username, reader)
+		} else if option == 3 {
+			createGroup(conn, username, reader)
+		} else {
+			joinGroup(conn, username, reader)
+		}
 	}
+}
+
+func createGroup(conn *websocket.Conn, from string, reader *bufio.Reader) {
+	fmt.Println("Enter group name:")
+	group, _ := reader.ReadString('\n')
+	group = group[:len(group)-1]
+
+	writeCommands(conn, reader, from, "", group, false, "create group")
+}
+
+func joinGroup(conn *websocket.Conn, from string, reader *bufio.Reader) {
+	fmt.Println("Enter group name:")
+	group, _ := reader.ReadString('\n')
+	group = group[:len(group)-1]
+
+	writeCommands(conn, reader, from, "", group, false, "join group")
 }
 
 func directMessage(conn *websocket.Conn, from string, reader *bufio.Reader) {
@@ -64,16 +89,16 @@ func directMessage(conn *websocket.Conn, from string, reader *bufio.Reader) {
 	to = to[:len(to)-1]
 
 	go readMessages(conn)
-	writeMessages(conn, reader, from, to, false)
+	writeMessages(conn, reader, from, to, "", false)
 }
 
 func groupMessage(conn *websocket.Conn, from string, reader *bufio.Reader) {
 	fmt.Println("Enter group name:")
-	to, _ := reader.ReadString('\n')
-	to = to[:len(to)-1]
+	group, _ := reader.ReadString('\n')
+	group = group[:len(group)-1]
 
 	go readMessages(conn)
-	writeMessages(conn, reader, from, to, true)
+	writeMessages(conn, reader, from, "", group, true)
 }
 
 func readMessages(conn *websocket.Conn) {
@@ -94,16 +119,24 @@ func readMessages(conn *websocket.Conn) {
 	}
 }
 
-func writeMessages(conn *websocket.Conn, reader *bufio.Reader, from string, to string, isGroupMsg bool) {
+func writeMessages(conn *websocket.Conn, reader *bufio.Reader, from string, to string, group string, isGroupMsg bool) {
 	for {
 		fmt.Print("> ")
 		text, _ := reader.ReadString('\n')
 		text = text[:len(text)-1] // Trim newline
 
-		err := conn.WriteJSON(Message{Type: "message", From: from, To: to, IsGroupMsg: isGroupMsg, Content: text})
+		err := conn.WriteJSON(Message{Type: "message", From: from, To: to, Group: group, IsGroupMsg: isGroupMsg, Content: text})
 		if err != nil {
 			fmt.Println("Write error:", err)
 			return
 		}
+	}
+}
+
+func writeCommands(conn *websocket.Conn, reader *bufio.Reader, from string, to string, group string, isGroupMsg bool, content string) {
+	err := conn.WriteJSON(Message{Type: "cmd", From: from, To: to, Group: group, IsGroupMsg: isGroupMsg, Content: content})
+	if err != nil {
+		fmt.Println("Write error:", err)
+		return
 	}
 }
