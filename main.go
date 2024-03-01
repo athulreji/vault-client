7cdcbdf016d2f5b3592ac22603945e9965f810d1
Athul Reji <athulreji007@gmail.com>
2024-03-02 01:44:02 +0530

    Adopted Bubble Tea for terminal UI

--- main.go
+++ main.go
@@ -7,8 +7,13 @@ import (
 	"net/url"
 	"os"
 
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/gorilla/websocket"
 )
+
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
 
 type Message struct {
 	Type       string `json:"type"`
@@ -19,7 +24,143 @@ type Message struct {
 	From       string `json:"From"`
 }
 
+var chatItems = make(map[string][]Message)
+
+func (item Message) Title() string {
+	return item.From
+}
+
+func (item Message) Description() string {
+	return item.Content
+}
+
+func (item Message) FilterValue() string {
+	return item.Content
+}
+
+type Chat struct {
+	name, desc string
+}
+
+func (item Chat) Title() string {
+	return item.name
+}
+
+func (item Chat) Description() string {
+	return item.desc
+}
+
+func (item Chat) FilterValue() string {
+	return item.name
+}
+
+type component int
+
+const (
+	chat component = iota
+	message
+	input
+)
+
+type state int
+
+const (
+	initializing state = iota
+	ready
+)
+
+type model struct {
+	height      int
+	width       int
+	chats       list.Model
+	messages    list.Model
+	currentChat string
+	focus       component
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case Message:
+		if _, ok := chatItems[msg.From]; !ok {
+			m.chats.InsertItem(0, Chat{msg.From, "today"})
+			if m.currentChat == "" {
+				m.currentChat = msg.From
+			}
+		}
+		chatItems[msg.From] = append(chatItems[msg.From], msg)
+		if m.currentChat == msg.From {
+			m.messages.InsertItem(len(m.messages.Items()), msg)
+		}
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q":
+			return m, tea.Quit
+		case "c":
+			m.focus = chat
+		case "m":
+			m.focus = message
+		case "/":
+			m.focus = input
+		}
+
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.height = msg.Height - h
+		m.width = msg.Width - v
+		m.chats.SetSize(m.width/3-1, m.height-4)
+		m.messages.SetSize((2*m.width/3)-1, m.height-4)
+	}
+
+	var (
+		cmd  tea.Cmd
+		cmds []tea.Cmd
+	)
+	m.chats, cmd = m.chats.Update(msg)
+	cmds = append(cmds, cmd)
+
+	m.messages, cmd = m.messages.Update(msg)
+	cmds = append(cmds, cmd)
+
+	return m, tea.Batch(cmds...)
+}
+
+func (m model) View() string {
+	var style = lipgloss.NewStyle().
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("#57288d"))
+
+	headview := style.Width(m.width).Height(1).Bold(true).Foreground(lipgloss.Color("#11bc7a")).Render(" Vault V0.1")
+	inputview := style.Width(m.width).Height(1).Foreground(lipgloss.Color("#11bc7a")).Render("----------------------------------------")
+	chatsview := style.Width(m.width/3 - 1).Height(m.height - 4).Render(m.chats.View())
+	messagesview := style.Width((2 * m.width / 3) - 1).Height(m.height - 4).Render(m.messages.View())
+	// head := "Vault v0.1"
+	return lipgloss.JoinVertical(lipgloss.Top, headview,
+		lipgloss.JoinHorizontal(lipgloss.Top, chatsview, messagesview),
+		inputview,
+	)
+}
+
 func main() {
+	m := model{chats: list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0), messages: list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0)}
+	m.chats.Title = "chats"
+	m.messages.Title = "messages"
+
+	p := tea.NewProgram(m, tea.WithAltScreen())
+	go readMessagesAlt(p)
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error creating bubble")
+	}
+
+}
+
+// var chats = make(map[string][]Message)
+
+func mainAlt() {
 	reader := bufio.NewReader(os.Stdin)
 
 	// Get username
@@ -42,6 +183,7 @@ func main() {
 		fmt.Println("Error sending username:", err)
 		return
 	}
+	go readMessages(conn)
 
 	for true {
 		// Listen for messages and send messages
@@ -65,6 +207,7 @@ func main() {
 			joinGroup(conn, username, reader)
 		}
 	}
+
 }
 
 func createGroup(conn *websocket.Conn, from string, reader *bufio.Reader) {
@@ -87,8 +230,6 @@ func directMessage(conn *websocket.Conn, from string, reader *bufio.Reader) {
 	fmt.Println("Enter senders username:")
 	to, _ := reader.ReadString('\n')
 	to = to[:len(to)-1]
-
-	go readMessages(conn)
 	writeMessages(conn, reader, from, to, "", false)
 }
 
@@ -96,9 +237,42 @@ func groupMessage(conn *websocket.Conn, from string, reader *bufio.Reader) {
 	fmt.Println("Enter group name:")
 	group, _ := reader.ReadString('\n')
 	group = group[:len(group)-1]
-
-	go readMessages(conn)
 	writeMessages(conn, reader, from, "", group, true)
+}
+func readMessagesAlt(p *tea.Program) {
+	username := "athul"
+
+	u := url.URL{Scheme: "ws", Host: "localhost:8080", Path: "/ws"}
+	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		return
+	}
+	defer conn.Close()
+
+	// Send username
+	err = conn.WriteJSON(Message{Type: "join", From: username})
+	if err != nil {
+		fmt.Println("Error sending username:", err)
+		return
+	}
+	for {
+		_, msgBytes, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Read error:", err)
+			return
+		}
+
+		var msg Message
+		err = json.Unmarshal(msgBytes, &msg)
+		if err != nil {
+			fmt.Println("Error decoding JSON:", err)
+			continue
+		}
+		p.Send(msg)
+		// chats[msg.From] = append(chats[msg.From], msg)
+		// fmt.Println(msg.From+":", msg.Content)
+	}
 }
 
 func readMessages(conn *websocket.Conn) {
@@ -115,6 +289,7 @@ func readMessages(conn *websocket.Conn) {
 			fmt.Println("Error decoding JSON:", err)
 			continue
 		}
+		// chats[msg.From] = append(chats[msg.From], msg)
 		fmt.Println(msg.From+":", msg.Content)
 	}
 }
